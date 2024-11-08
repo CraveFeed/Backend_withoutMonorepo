f87e6d8f06304e0a092ebb6ff8ef207ad52a53f2
Shashwat Singh <shashwat123student@gmail.com>
2024-11-08 06:52:13 +0530

    chat app part done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import userRoutes from './routes/userRoute';
 import emailVerificationRoutes from './routes/emailVerification';
+import chatRoutes from './routes/chatRoutes';
+import {initializeChatSocket} from "./loaders/webSockets/chatSocket";
+import {authenticateUser} from "./middlewares/authMiddleware";
 
 const app = express();
 app.use(express.json());
 
 app.use('/', emailVerificationRoutes);
 app.use('/user', userRoutes);
+app.use(authenticateUser);
+app.use('/chat', chatRoutes);
+
+initializeChatSocket(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 import * as authController from "../controllers/auth"
-import { authenticateUser } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post('/signUp', authController.register)
 router.post('/signIn', authController.login)
-
-router.use(authenticateUser);
 
 export default router;
